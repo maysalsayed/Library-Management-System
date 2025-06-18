e28c98837b0ffb0c8afb8dc708381a97ff3ebecd
maysalsayed <maysalsayed87@gmail.com>
2025-06-18 11:01:03 +0300

    9th committ

--- src/Main.java
+++ src/Main.java
@@ -95,4 +95,61 @@ public class Main{
         String [] returned = {BookTitle,AuthorName,ISBN};
         return returned;
     }
+
+    public static void ManageMembers() throws IOException{
+        System.out.println("""
+        --- Book Management ---
+        Choose the number of the choice:
+        1. Add a Member
+        2. Remove a Member
+        3. Update Member Details
+        4. List all Members
+        5. Back to Main Menu
+        """);
+        int userAnswer = scanner.nextInt();
+        scanner.nextLine();
+        Member memberOperations = new Member();
+        String [] returned;
+        if (userAnswer == 1){
+            returned = getMemberInfo();
+            System.out.println(memberOperations.addMember(returned[0], returned[1], returned[2]));
+        }
+        else if(userAnswer == 2){
+            returned = getMemberInfo();
+            System.out.println(memberOperations.removeBook(returned[0],returned[1],returned[2]));
+        }
+        else if (userAnswer == 3){
+            returned = getBookInfo();
+            System.out.println("""
+            What to modify?
+            Choose the number of the choice:
+            1.Book Title
+            2.Book Author
+            3.Book ISBN
+            """);
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Enter the new info needed to modify: ");
+            String bookInfoToChange = scanner.nextLine();
+            System.out.println(bookOperations.editBook(returned[0],returned[1],returned[2],choice,bookInfoToChange));
+        }
+        else if (userAnswer == 4){
+            bookOperations.listBooks();
+        }
+        else if (userAnswer == 5){
+
+        }
+
+    }
+
+    public static String [] getMemberInfo(){
+        System.out.println("Enter Member Name: ");
+        String memberName = scanner.nextLine();
+        System.out.println("Enter Member Email: ");
+        String memberEmail = scanner.nextLine();
+        System.out.println("Enter Member Phone: ");
+        String memberPhone = scanner.nextLine();
+        String [] returned = {memberName, memberEmail, memberPhone};
+        return returned;
+    }
 }
--- src/Member.java
+++ src/Member.java
@@ -1,15 +1,37 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
 
     private int MemberID;
     private String MemberName;
     private String MemberEmail;
     private String MemberPhone;
+    String MembersFileName = "Membes.txt";
+    String [] arrMembers;
+    public Member() throws IOException {
+        try {
+            File file = new File(MembersFileName);
 
-    public Member(int MemberID, String MemberName,String MemeberEmail, String MemberPhone){
-        setMemberID(MemberID);
-        setMemberName(MemberName);
-        setMemberEmail(MemeberEmail);
-        setMemberPhone(MemberPhone);
+            if (!file.exists()) {
+                // Create the file (empty)
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                writer.close(); // Just create and close
+            }
+            FileReader readerr = new FileReader(MembersFileName);
+            BufferedReader reader = new BufferedReader(readerr);
+
+            List<String> lines = new ArrayList<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+            this.arrMembers = lines.toArray(new String[0]);
+            reader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public int getMemberID(){
@@ -35,5 +57,37 @@ public class Member {
     }
     public void setMemberPhone(String MemberPhone){
         this.MemberPhone = MemberPhone;
+    }
+
+    public void writeData(String[] arr) throws IOException {
+        FileWriter file = new FileWriter(MembersFileName);
+        BufferedWriter writer = new BufferedWriter(file);
+        for (String s : arr) {
+            writer.write(s + "\n");
+        }
+        writer.close();
+    }
+
+    public String addMember(String MemberName, String MemberEmail, String MemberPhone) throws IOException {
+        for (String s : this.arrMembers) {
+            if (this.arrMembers.length == 1 && this.arrMembers[0] == null)
+                break;
+            else {
+                String[] splittedMember = s.split(",");
+                if (splittedMember[1].equalsIgnoreCase(MemberName) && splittedMember[2].equalsIgnoreCase(MemberEmail) && splittedMember[3].equalsIgnoreCase(MemberPhone)) {
+                    return "The Member already exists";
+                }
+            }
+        }
+        String[] Members = new String[this.arrMembers.length + 1];
+        //listOfBooks.add(book);
+        for (int i = 0; i < Members.length; i++) {
+            if (i != Members.length - 1)
+                Members[i] = this.arrMembers[i];
+            else
+                Members[i] = Members.length+1 + "," + MemberName + "," + MemberEmail + "," + MemberPhone;
+        }
+        this.writeData(Members);
+        return "Book Added Successfully!";
     }
 }
